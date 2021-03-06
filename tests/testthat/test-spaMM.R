26ff536c1402539dae82ccf5a2c2063b3a9ee983
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2021-03-06 17:37:48 +0100

    Add skeleton for tests

---
+++ tests/testthat/test-spaMM.R
@@ -0,0 +1,9 @@
+stopifnot(require("testthat"), require("spaMM"))
+## test tidy, augment, glance methods from spaMM_tidiers.R
+
+## tests for tidy()
+
+## tests for glance()
+
+## tests for augment()
+
